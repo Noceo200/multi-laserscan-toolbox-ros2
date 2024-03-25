17e5fc99d54bf1d72cd09b6c94d3d867ccb7b224
Noceo200 <onoel2050@gmail.com>
2024-03-25 13:39:24 +0900

    Update laserscan_toolbox.cpp

--- src/laserscan_toolbox.cpp
+++ src/laserscan_toolbox.cpp
@@ -16,12 +16,12 @@ geometry_msgs::msg::Vector3 stringToVector3(std::string& str);
 geometry_msgs::msg::Vector3 quaternion_to_euler(geometry_msgs::msg::Quaternion quat);
 geometry_msgs::msg::Vector3 adapt_angle(geometry_msgs::msg::Vector3 vect);
 double TimeToDouble(builtin_interfaces::msg::Time& stamp);
-sensor_msgs::msg::LaserScan::SharedPtr convert_raw_data(sensor_msgs::msg::LaserScan::SharedPtr laser_raw,std::string new_frame,double start_angle, double end_angle, double angle_origin_offset, geometry_msgs::msg::Vector3 vector_newframe, geometry_msgs::msg::Vector3 rotate_newframe);
+sensor_msgs::msg::LaserScan::SharedPtr convert_raw_data(sensor_msgs::msg::LaserScan::SharedPtr laser_raw,std::string new_frame,double start_angle, double end_angle, double angle_origin_offset, geometry_msgs::msg::Vector3 vector_newframe, geometry_msgs::msg::Vector3 rotate_newframe, std::stringstream &debug_ss);
 sensor_msgs::msg::LaserScan filter_data(sensor_msgs::msg::LaserScan::SharedPtr msg,double start_angle, double end_angle, double angle_origin_offset);
 int angle_to_index(double alpha, int resolution);
 double index_to_angle(int ind, int resolution);
 void get_pos(double &x, double &y,double alpha,double val,double x_off,double y_off);
-sensor_msgs::msg::LaserScan transform_data(sensor_msgs::msg::LaserScan::SharedPtr msg, double off_vect_x,double off_vect_y, double off_tetha);
+sensor_msgs::msg::LaserScan transform_data(sensor_msgs::msg::LaserScan::SharedPtr msg, double off_vect_x,double off_vect_y, double off_tetha, std::stringstream &debug_ss);
 
 
 class LaserScanToolboxNode : public rclcpp::Node {
@@ -134,7 +134,7 @@ public:
             //Scan transformations and processing according to configurations
             for (const auto& pair1 : raw_scans) {
                 std::string source_name = pair1.first;
-                transformed_scans[source_name] = convert_raw_data(raw_scans[source_name],new_frame,std::stod(sources_config[source_name]["start_angle"]), std::stod(sources_config[source_name]["end_angle"]),std::stod(sources_config[source_name]["scan_angle_offset"]), stringToVector3(sources_var[source_name]["frame_trans_vector"]), stringToVector3(sources_var[source_name]["frame_rot_vector"]));
+                transformed_scans[source_name] = convert_raw_data(raw_scans[source_name],new_frame,std::stod(sources_config[source_name]["start_angle"]), std::stod(sources_config[source_name]["end_angle"]),std::stod(sources_config[source_name]["scan_angle_offset"]), stringToVector3(sources_var[source_name]["frame_trans_vector"]), stringToVector3(sources_var[source_name]["frame_rot_vector"]), debug_ss);
                 debug_ss << source_name << ": Scan transformed, ready for fusion..." << "(last data stamp: " << sources_var[source_name]["last_timestamp"] << " s) (node time: " << (this->now()).nanoseconds() << "ns)" << std::endl;
             }
 
@@ -197,7 +197,11 @@ public:
         //merge scan2 in scan1
         //fuse datas
         for (int i = 0; i < resolution; ++i) { 
-            scan1.ranges[i] = std::min(scan1.ranges[i],scan2.ranges[i]); //we take the closest signal
+            //we take the closest signal
+            if(scan2.ranges[i]<scan1.ranges[i]){
+                scan1.ranges[i] = scan2.ranges[i];
+                scan1.intensities[i] = scan2.intensities[i];
+            }
         }
     }
 
@@ -225,6 +229,7 @@ public:
         clean_scan.ranges = {};
         for (int i = 0; i < resolution; ++i){
             clean_scan.ranges.push_back(INFINITY);
+            clean_scan.intensities.push_back(0.0);
         }
 
         return clean_scan;
@@ -473,12 +478,13 @@ double TimeToDouble(builtin_interfaces::msg::Time& stamp){
     return static_cast<double>(stamp.sec) + static_cast<double>(stamp.nanosec) * 1e-9;
 }
 
-sensor_msgs::msg::LaserScan::SharedPtr convert_raw_data(sensor_msgs::msg::LaserScan::SharedPtr laser_raw,std::string new_frame,double start_angle, double end_angle, double angle_origin_offset, geometry_msgs::msg::Vector3 vector_newframe, geometry_msgs::msg::Vector3 rotate_newframe){
+sensor_msgs::msg::LaserScan::SharedPtr convert_raw_data(sensor_msgs::msg::LaserScan::SharedPtr laser_raw,std::string new_frame,double start_angle, double end_angle, double angle_origin_offset, geometry_msgs::msg::Vector3 vector_newframe, geometry_msgs::msg::Vector3 rotate_newframe, std::stringstream &debug_ss){
     if (laser_raw != nullptr){
         //RCLCPP_INFO(this->get_logger(), "Lid1_run");
+        //debug_ss << "/nDEBUG: vectx_= " << vector_newframe.x << "  vecty_= " << vector_newframe.y << "angle_= " << rotate_newframe.z << std::endl;
         //transform data and configure new message
         sensor_msgs::msg::LaserScan tempo = filter_data(laser_raw,start_angle, end_angle, angle_origin_offset);
-        sensor_msgs::msg::LaserScan tempo2 = transform_data(std::make_shared<sensor_msgs::msg::LaserScan>(tempo),vector_newframe.x,vector_newframe.y,rotate_newframe.z);
+        sensor_msgs::msg::LaserScan tempo2 = transform_data(std::make_shared<sensor_msgs::msg::LaserScan>(tempo),vector_newframe.x,vector_newframe.y,rotate_newframe.z,debug_ss);
         sensor_msgs::msg::LaserScan::SharedPtr scan_data_ = std::make_shared<sensor_msgs::msg::LaserScan>(tempo2); 
         //RCLCPP_INFO(this->get_logger(), "Lid1_transformed");
         scan_data_->header=laser_raw->header;
@@ -512,27 +518,9 @@ bool consider_val(int current_ind, int start_ind, int end_ind){
 }
 
 sensor_msgs::msg::LaserScan filter_data(sensor_msgs::msg::LaserScan::SharedPtr msg,double start_angle, double end_angle, double angle_origin_offset){
-
-    sensor_msgs::msg::LaserScan filtered_scan;
-    filtered_scan.ranges.clear();
     int resolution = msg->ranges.size();
-
-    //compute index to consider according to min/max angles wanted
-    int start_index = angle_to_index(start_angle+angle_origin_offset, resolution);
-    int end_index = angle_to_index(end_angle+angle_origin_offset, resolution);
-
-    // LaserScan message filling
-    for (int i = 0; i < resolution; ++i) { 
-        if(consider_val(i, start_index, end_index)){ //if the value is autorized, we add it
-            filtered_scan.ranges.push_back(msg->ranges[i]);
-        }
-        else{
-            filtered_scan.ranges.push_back(INFINITY);
-        }
-    }
-
-    //we shift all the values to bring back the origin angle to the x axis (not needed)
-    /*int index_shift = angle_to_index(angle_origin_offset,resolution); //index which is the first angle for the lidar
+    //we shift all the values to bring back the origin angle to the x axis
+    int index_shift = angle_to_index(angle_origin_offset,resolution); //index which is the first angle for the lidar
     sensor_msgs::msg::LaserScan shifted_scan;
     shifted_scan.ranges.clear();
     for (int i = 0; i < resolution; ++i) { 
@@ -542,7 +530,25 @@ sensor_msgs::msg::LaserScan filter_data(sensor_msgs::msg::LaserScan::SharedPtr m
         else{
             shifted_scan.ranges.push_back(msg->ranges[i+index_shift-resolution]);
         }
-    }*/
+    }
+
+    //we keep the wanted angles
+    sensor_msgs::msg::LaserScan filtered_scan;
+    filtered_scan.ranges.clear();
+
+    //compute index to consider according to min/max angles wanted
+    int start_index = angle_to_index(start_angle, resolution);
+    int end_index = angle_to_index(end_angle, resolution);
+
+    // LaserScan message filling
+    for (int i = 0; i < resolution; ++i) { 
+        if(consider_val(i, start_index, end_index)){ //if the value is autorized, we add it
+            filtered_scan.ranges.push_back(shifted_scan.ranges[i]);
+        }
+        else{
+            filtered_scan.ranges.push_back(INFINITY);
+        }
+    }
 
     return filtered_scan;
 }
@@ -567,14 +573,111 @@ void get_pos(double &x, double &y,double alpha,double val,double x_off,double y_
     y = val*sin(alpha)+y_off;
 }
 
-bool save_dataset = false;//if true, the code will generate a dataset with the first values of the first lidar in home/scan_merger_dataset.txt
-sensor_msgs::msg::LaserScan transform_data(sensor_msgs::msg::LaserScan::SharedPtr msg, double off_vect_x,double off_vect_y, double off_tetha){
+sensor_msgs::msg::LaserScan transform_data(sensor_msgs::msg::LaserScan::SharedPtr msg, double off_vect_x,double off_vect_y, double off_tetha, std::stringstream &debug_ss){
     /*
     msg: current message to transform
     off_vect_x: vector new_origin=>lidar
     off_vect_y: vector new_origin=>lidar
     return transformed_scan: message in which datas will be stored
+    Homogoneous transformations: https://www.fil.univ-lille.fr/portail/archive21-22/~aubert/m3d/m3d_transformation.pdf
     */
+    //RCLCPP_INFO(this->get_logger(), "Transform_data: params : offx='%f'; offy='%f'; off_tetha='%f'",off_vect_x,off_vect_y,off_tetha);
+    //RCLCPP_INFO(this->get_logger(), "transform 1");
+    sensor_msgs::msg::LaserScan transformed_scan;
+    transformed_scan.ranges.clear();
+    int resolution = msg->ranges.size();
+
+    // Transform the LaserScan message
+    for (int i = 0; i < resolution; ++i) { //initiate
+        transformed_scan.ranges.push_back(INFINITY);
+    }
+
+    //let's be sure to manipulate angles from 0 to 2pi
+    if (off_tetha<0){
+        off_tetha=2*M_PI+off_tetha;
+    }
+    if (off_tetha == 2*M_PI){
+        off_tetha=0;
+    }
+    //translation in homogeneous coordinates
+    Eigen::MatrixXd T = Eigen::MatrixXd::Identity(4, 4);
+    T(0,3) = off_vect_x;
+    T(1,3) = off_vect_y;
+    //rotation in homogeneous coordinates
+    Eigen::MatrixXd R = Eigen::MatrixXd::Identity(4, 4);
+    R(0,0) = cos(off_tetha);
+    R(0,1) = -sin(off_tetha);
+    R(1,0) = sin(off_tetha);
+    R(1,1) = cos(off_tetha);
+    //final homogeneous transformation, M1_2 = pass matrix, displacement from new_frame to sensor_frame
+    Eigen::MatrixXd M1_2(4, 4);
+    M1_2 = T*R;
+
+    //debug_ss << "DEBUG: Pass Matrix:" << std::endl;
+    //debug_ss << M1_2 << std::endl;
+
+    //loop variables
+    double init_x = 0;
+    double init_y = 0;
+    double init_val = 0;
+    double init_angle = 0;
+    double new_x = 0;
+    double new_y = 0;
+    double new_val = 0;
+    double new_angle = 0;
+    int new_index = 0;
+
+    for (int ind = 0; ind < resolution; ++ind) { //transform
+        init_angle = index_to_angle(ind,resolution);
+        init_val = msg->ranges[ind];
+
+        //points transformation
+        if (!isinf(init_val)){ //if it is infinity, we don't need to compute as the default values in the new scan will be INFINITY
+            get_pos(init_x, init_y,init_angle,init_val,0.0,0.0); //position in inital frame, vector: lidar=>point
+            Eigen::MatrixXd X2(4, 1); //pos in sensor frame
+            X2(0,0) = init_x;
+            X2(1,0) = init_y;
+            X2(2,0) = 0.0;
+            X2(3,0) = 1.0;
+            Eigen::MatrixXd X1(4, 1); //pos in newframe
+            X1 = M1_2*X2;
+            //Unpack
+            double new_x2 = X1(0,0);
+            double new_y2 = X1(1,0);
+            new_angle = atan2(new_y2,new_x2);
+            //we want angles from 0 to 2pi
+            if (new_angle<0){
+                new_angle=2*M_PI+new_angle;
+            }
+            if (new_angle == 2*M_PI){
+                new_angle=0;
+            }
+            //RCLCPP_INFO(this->get_logger(), "Transform_data: newangle='%f'",new_angle);
+
+            //new norm
+            new_val = sqrt(pow(new_x2,2)+pow(new_y2,2));
+
+            new_index = angle_to_index(new_angle,resolution);
+            transformed_scan.ranges[new_index] = std::min(static_cast<double>(transformed_scan.ranges[new_index]),new_val); //in case some area are no more accessible from the new origin
+            //RCLCPP_INFO(this->get_logger(), "Transform_data: initindex='%d'; initangle='%f'; initval='%f'; initx='%f'; inity='%f'; newx2='%f'; newy2='%f'; newval='%f'; newangle='%f'; newindex='%f'",ind,init_angle,init_val,init_x,init_y,new_x2,new_y2,new_val,new_angle,new_index);
+        }
+
+    }
+    //RCLCPP_INFO(this->get_logger(), "transform 3");
+
+    return transformed_scan;
+}
+
+////TRASH
+
+/*bool save_dataset = false;//if true, the code will generate a dataset with the first values of the first lidar in home/scan_merger_dataset.txt
+sensor_msgs::msg::LaserScan transform_data(sensor_msgs::msg::LaserScan::SharedPtr msg, double off_vect_x,double off_vect_y, double off_tetha){
+    
+    //msg: current message to transform
+    //off_vect_x: vector new_origin=>lidar
+    //off_vect_y: vector new_origin=>lidar
+    //return transformed_scan: message in which datas will be stored
+    
     //RCLCPP_INFO(this->get_logger(), "Transform_data: params : offx='%f'; offy='%f'; off_tetha='%f'",off_vect_x,off_vect_y,off_tetha);
     //RCLCPP_INFO(this->get_logger(), "transform 1");
     sensor_msgs::msg::LaserScan transformed_scan;
@@ -649,4 +752,4 @@ sensor_msgs::msg::LaserScan transform_data(sensor_msgs::msg::LaserScan::SharedPt
     } 
 
     return transformed_scan;
-}
+}*/
